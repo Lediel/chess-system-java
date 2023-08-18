073f44fa5bb9e0cbc20e70083760b98cf3debc1d
Lediel da Rocha Delfino <lediel.delfino@betha.com.br>
2023-08-18 08:02:07 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,15 +3,33 @@ package application;
 import boardgame.Board;
 import boardgame.Position;
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
-        UI.printBoard(chessMatch.getPieces());
 
-        //Board board = new Board(8, 8);
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+
+            System.out.println();
+            System.out.print("Target:");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece captuderPiece = chessMatch.performChessMove(source, target);
+        }
+
+
+
 
     }
 }
